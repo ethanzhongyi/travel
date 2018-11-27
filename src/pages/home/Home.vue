35701fab118867ffd6341240ebe632676baa8cce
Sayid <sayidzhong@126.com@zhongyideMac.local>
2018-11-27 21:04:02 +0800

    首页轮播图

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,13 +1,19 @@
 <template>
-  <home-header>hello world</home-header>
+  <div>
+    <home-header>hello world</home-header>
+    <home-swiper>swiper</home-swiper>
+    <div>test</div>
+  </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
 export default {
   name: 'Home',
   components: {
-    HomeHeader: HomeHeader
+    HomeHeader: HomeHeader,
+    HomeSwiper: HomeSwiper
   }
 }
 </script>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,45 @@
+<template>
+<div class='wrapper'>
+  <swiper :options="swiperOption">
+    <!-- slides -->
+    <swiper-slide v-for='item of swiperList' :key='swiperList.id'>
+      <img class='swiper-img' :src='item.imgUrl' />
+    </swiper-slide>
+    
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data: function () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/8c/b9e86352f9aa5f02.jpg_750x200_2fe2875f.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/4d/6706bf772c05ce02.jpg_750x200_773e75f0.jpg'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/61/44400d6591891202.jpg_750x200_7565a05c.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+    .wrapper >>> .swiper-pagination-bullet-active
+      background: #fff
+    .swiper-img
+      width: 100%
+
+</style>
