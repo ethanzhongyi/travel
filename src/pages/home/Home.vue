d25dc64570c866be17ac335394b204e5b198be41
Sayid <sayidzhong@126.com@zhongyideMac.local>
2018-11-28 07:00:51 +0800

    图标区域布局

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,19 +1,22 @@
 <template>
   <div>
-    <home-header>hello world</home-header>
-    <home-swiper>swiper</home-swiper>
-    <div>test</div>
+    <home-header></home-header>
+    <home-swiper></home-swiper>
+    <home-icons></home-icons>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
+
 export default {
   name: 'Home',
   components: {
     HomeHeader: HomeHeader,
-    HomeSwiper: HomeSwiper
+    HomeSwiper: HomeSwiper,
+    HomeIcons: HomeIcons
   }
 }
 </script>
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -2,10 +2,9 @@
 <div class='wrapper'>
   <swiper :options="swiperOption">
     <!-- slides -->
-    <swiper-slide v-for='item of swiperList' :key='swiperList.id'>
+    <swiper-slide v-for='item of swiperList' v-bind:key='item.id'>
       <img class='swiper-img' :src='item.imgUrl' />
     </swiper-slide>
-    
     <!-- Optional controls -->
     <div class="swiper-pagination"  slot="pagination"></div>
   </swiper>
