ff31fd57fcc9a461c66ee4a88962d1d2fd1c77cf
sayid <sayidzhong@126.com>
2018-11-29 19:26:12 +0800

    7-7 Vue项目首页 - 开发周末游组件

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <div class='title'>周末去哪儿</div>
+	<ul>
+	  <li class='item' 
+	    v-for='item of recommendList'
+	    :key='item.id'
+	  >
+	    <div class='item-img-wrapper'>
+	      <img class='item-img' :src='item.imgUrl'/>
+	    </div>
+	    <div class='item-info'>
+	      <p class='item-title'>{{item.title}}</p>
+	      <p class='item-desc'>{{item.desc}}</p>
+	    </div>
+	  </li>
+	</ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeWeekend',
+    data () {
+      return {
+        recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg',
+        title: '京城人的周末撒欢地',
+        desc: '在帝都过周末，不仅仅是城中游！'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/7e/476589267ebb41.jpg_r_640x214_bf599709.jpg',
+        title: '京城欢乐亲子游',
+        desc: '德智体美劳全面发展的亲子日，这些地方该去看看…'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg',
+        title: '京城有好泉',
+        desc: '细数北京温泉，温暖你的冬天'
+      }]
+      }
+    }
+}
+</script>
+
+<style lang='stylus' scoped>
+  .title
+      margin-top: .2rem
+      line-height: .8rem
+      background: #eee
+      text-indent: .2rem
+  .item-img-wrapper
+      overflow: hidden
+      height: 0
+      padding-bottom: 33.9%
+  .item-img
+      width: 100%
+  .item-info
+    flex: 1
+    padding: .1rem
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+    .iten-desc
+      line-height: .4rem
+      color: #ccc
+</style>
