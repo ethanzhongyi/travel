316645ff406213cb8d32789407932d163a07778c
zhongyi <zhongyi@giec.com>
2018-12-02 15:57:16 +0800

    优化字母表联动

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -32,8 +32,13 @@ export default {
   },
   data () {
     return {
-      touchStatus: false
+      touchStatus: false,
+      startY: 0,
+      timer: null
     }
+  },
+  updated () {
+    this.startY = this.$refs['A'][0].offsetTop
   },
   methods: {
     handleLetterClick (e) {
@@ -44,12 +49,16 @@ export default {
     },
     handleTouchMove (e) {
       if (this.touchStatus) {
-        const startY = this.$refs['A'][0].offsetTop
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      this.timer = setTimeout(() => {
         const touchY = e.touches[0].clientY - 79
-        const index = Math.floor((touchY - startY) / 20)
+        const index = Math.floor((touchY - this.startY) / 20)
         if (index >= 0 && index < this.letters.length) {
           this.$emit('change', this.letters[index])
         }
+      },16)        
       }
     },
     handleTouchEnd () {
